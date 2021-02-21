83590482dbca8501df7eccd62498e7f258c7e493
Jayden Randhawa <jaydenr917@icloud.com>
2021-02-21 18:43:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var io = require('socket.io')(http);
 
 const port = 8080;
 
+var conUsers = 0;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -15,15 +17,38 @@ io.on('connection', function (socket) {
   console.log(`${clientIp} has connected`);
   io.emit('system message', `SYSTEM:${cliIp} has joined.`)
 
+  conUsers += 1;
+  console.log(conUsers);
+
+  if(conUsers == 1){
+    io.emit('system message', `It looks like you are the only one here...`);
+  }
+  else{
+    io.emit('system message', `There are now ${conUsers} people online`);
+  }
+
   socket.on('chat message', (m) => {
     console.log(`${clientIp} :${m}`);
-    io.emit('chat message', `${cliIp}:${m}`);
+    if(m != ""){
+      io.emit('chat message', `${cliIp}:${m}`);
+    }
   });
 
   socket.on('disconnect', () => {
     console.log(`${clientIp} has disconnected`)
-    io.emit('system message', `SYSTEM:${cliIp} has left.`)
-  })
+    io.emit('system message', `SYSTEM:${cliIp} has left.`);
+
+    conUsers -= 1;
+    console.log(conUsers);
+
+    if(conUsers == 1){
+      io.emit('system message', `It looks like you are the only one here...`);
+    }
+    else{
+      io.emit('system message', `There are now ${conUsers} people online`);
+    }
+
+  });
 });
 
 http.listen(port, () => {
